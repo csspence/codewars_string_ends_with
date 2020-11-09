5df138650fac79bce5f7c3839e0a8bbcc7f99476
Crew Spence <crewspence@gCrewSpence.local>
2020-11-09 05:16:56 -0600

    Refactored solution after learning about endsWith method.

--- codewars_string_ends_with.js
+++ codewars_string_ends_with.js
@@ -8,14 +8,5 @@ solution('abc', 'd') // returns false
 */
 
 const solution = (str, ending) => {
-  let start = str.length - ending.length;
-  let index = 0;
-  for(let i = start; i < str.length; i++) {
-    if(str[i] !== ending[index]) {
-      return false;
-    }
-    index++;
-  }
-
-  return true;
+  return str.endsWith(ending);
 }
