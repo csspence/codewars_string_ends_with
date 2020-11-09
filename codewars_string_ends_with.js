9cf37a4a755ea94dbb290ec02cca0da6f877302b
Crew Spence <crewspence@gCrewSpence.local>
2020-11-09 05:14:43 -0600

    Wrote working solution.

--- codewars_string_ends_with.js
+++ codewars_string_ends_with.js
@@ -7,6 +7,15 @@ solution('abc', 'bc') // returns true
 solution('abc', 'd') // returns false
 */
 
-function solution(str, ending){
-  // TODO: complete
+const solution = (str, ending) => {
+  let start = str.length - ending.length;
+  let index = 0;
+  for(let i = start; i < str.length; i++) {
+    if(str[i] !== ending[index]) {
+      return false;
+    }
+    index++;
+  }
+
+  return true;
 }
